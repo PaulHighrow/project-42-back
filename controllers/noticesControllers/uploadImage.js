1fb23cf5c9cc5c66b369258a16f843cc1bad9144
Artem Maslii <a.masliy@ukr.net>
2023-05-21 22:13:36 +0300

    Update uploadImage.js

--- controllers/noticesControllers/uploadImage.js
+++ controllers/noticesControllers/uploadImage.js
@@ -1,16 +1,16 @@
-const cloudinary = require('cloudinary').v2;
+// const cloudinary = require('cloudinary').v2;
 
-const uploadImage = async (req, res) => {
-  console.log(req.file);
-  const imgId = req.file ? req.file.filename : req.user.imgId;
-  const result = await cloudinary.uploader.upload(imgId, {
-    public_id: 'cat',
-  });
-  res.json({
-    status: 'success',
-    code: 200,
-    data: result,
-  });
-};
+// const uploadImage = async (req, res) => {
+//   console.log(req.file);
+//   const imgId = req.file ? req.file.filename : req.user.imgId;
+//   const result = await cloudinary.uploader.upload(imgId, {
+//     public_id: 'cat',
+//   });
+//   res.json({
+//     status: 'success',
+//     code: 200,
+//     data: result,
+//   });
+// };
 
-module.exports = uploadImage;
+// module.exports = uploadImage;
